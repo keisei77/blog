8ef2c42c0f0da0dbd37fc2073651e84b6b7a72a8
Keisei Jia <keisei.jia@qq.com>
2019-12-19 00:02:41 +0800

    :wrench: update force deploy script

--- scripts/force-deploy.sh
+++ scripts/force-deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-git stash;
 git checkout gh-pages;
 git fetch origin gh-pages;
 rm -rf public/*;
-git add -A;
+git add -u;
 git commit -m ":package: force update";
 git push --force origin gh-pages;
 git checkout master;
-git stash pop;
