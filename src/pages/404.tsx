79ffe691724fbb3c4a3a61a924597b761db17eeb
Keisei Jia <keisei.jia@qq.com>
2020-01-01 22:19:46 +0800

    :recycle: transform 404 page from js to tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,20 +4,31 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
+interface NotFoundPageProps {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string;
+      };
+    };
+  };
+  location: string;
+}
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
+const NotFoundPage = (props: NotFoundPageProps) => {
+  const { data } = props;
+  const siteTitle = data.site.siteMetadata.title;
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    );
-  }
-}
+      </>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
 
