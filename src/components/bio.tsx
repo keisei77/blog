3b16a77987b87455f209e37cd45e8ec89d396fb3
keisei77 <keisei.jia@qq.com>
2020-03-18 20:18:52 +0800

    :bento: update self info

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -6,6 +6,8 @@
  */
 
 import React from 'react';
+import certificate1 from '../assets/20200318195904.jpg';
+import certificate2 from '../assets/20200318195855.jpg';
 import { useStaticQuery, graphql } from 'gatsby';
 
 const Bio = () => {
@@ -31,16 +33,46 @@ const Bio = () => {
         paddingTop: '1rem',
       }}
     >
-      <div>
-        前端开发者一枚，喜欢看电影，游戏，篮球，更喜欢写代码。偶尔做做公益，学习新技术。
+      <div
+        style={{
+          paddingBottom: '10px',
+        }}
+      >
+        前端开发者一枚，喜欢看电影，游戏，篮球，更喜欢写代码。偶尔做做公益，参加一些技术沙龙，学习新技术。
       </div>
-      <div>
+      <div
+        style={{
+          paddingBottom: '10px',
+        }}
+      >
         <a href={`mailto:${email}`}>{email}</a>
       </div>
       <div>
         <a href={github} target="_blank" rel="noopener noreferrer">
           {github}
         </a>
+      </div>
+      <div
+        style={{
+          textAlign: 'center',
+          padding: '20px',
+        }}
+      >
+        <img
+          style={{
+            maxWidth: '300px',
+            paddingBottom: '10px',
+          }}
+          src={certificate1}
+          alt="证书"
+        />
+        <img
+          style={{
+            maxWidth: '300px',
+          }}
+          src={certificate2}
+          alt="证书"
+        />
       </div>
     </div>
   );
